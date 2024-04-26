c4b649403d8cb1d59fa1c04535528864604ae260
Take-git59 <76654198+Take-git59@users.noreply.github.com>
2024-04-26 10:46:01 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/config.h
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
 // Key matrix parameters
 #define MATRIX_ROWS         (4 * 2)  // split keyboard
